3bc079028ba058d305cf90cd463b140b1a7d4448
Adis16 <adarshdixit492@gmail.com>
2024-07-06 03:24:50 +0530

    friday morning

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&ssl=false";
+
+const connectToMongo = async()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected to mongoose successfully");
+
+
+    })
+
+}
+
+module.exports = connectToMongo;
