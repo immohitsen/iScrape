3f67236ae9ecc11e274a24a4ed9d2a9ac8c8686c
immohitsen <senmohit9005@gmail.com>
2024-11-14 04:24:16 +0530

    deployenv1

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "https://iscrape.vercel.app/";
+  const host = process.env.HOST;
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 
