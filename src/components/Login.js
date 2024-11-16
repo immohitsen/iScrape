b45bb28349e75ed4f365dabf8bf4be78f49af8ec
Mohit Sen <86571773+immohitsen@users.noreply.github.com>
2024-11-17 04:11:44 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login(props) {
-  const host = "https://iscrape.vercel.app/";
+  const host = process.env.HOST;
 
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   let navigate = useNavigate();
