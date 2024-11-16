bed451483d4532961b42b134f4e4e67d868fc603
Mohit Sen <86571773+immohitsen@users.noreply.github.com>
2024-11-17 04:12:02 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = (props) => {
-  const host = "https://iscrape.vercel.app/";
+  const host = process.env.HOST;
 
   const [credentials, setCredentials] = useState({
     name: "",
